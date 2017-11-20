008c3c3cab1dfbfffd9d291112e2de4a3ccd0df0
Flip <philippe@pilarkto.net>
2017-11-20 17:37:14 +0100

    update image names

--- site.mk
+++ site.mk
@@ -46,7 +46,7 @@ GLUON_SITE_PACKAGES += \
 	kmod-igb
 endif
 
-DEFAULT_GLUON_RELEASE := 1.4+$(shell date '+%Y-%m-%d')-g.$(shell git -C $(GLUONDIR) log --pretty=format:'%h' -n 1)-s.$(shell git -C $(GLUONDIR)/site log --pretty=format:'%h' -n 1)
+DEFAULT_GLUON_RELEASE := 1.4+$(shell date '+%Y-%m-%d')
 
 GLUON_LANGS := de en
 
